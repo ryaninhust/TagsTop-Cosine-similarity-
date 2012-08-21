d81539a24193f768f9e927674eb30ca2a654fd43
ryaninhust <yuanbwhust@gmail.com>
2012-08-21 12:11:41 +0800

    finish the mongo module

--- persist_mongo.py
+++ persist_mongo.py
@@ -1,13 +1,14 @@
 from pymongo import Connection
 from group_pv import init, get_dict_files
-#from word_similarity import _simple_divide, merge_tags_rdd
+from word_similarity import _simple_divide, merge_tags_rdd
 
 DB = Connection().tagtop
 
 
-def save_documents(collection, formated_rdd):
+def save_documents(collection, formated_rdd, splitSize = 2):
     collection.remove()
-    collection.insert(formated_rdd.collect())
+    for i in range (splitSize):
+        collection.insert(formated_rdd.collect()[i::splitSize])
 
 
 def format_rdd(format_func, data_files):
@@ -17,14 +18,37 @@ def format_rdd(format_func, data_files):
 
 
 def _relative_format(line):
-    line = line.replace('.','_')
+    line = line.replace('.', '_')
     return {line.strip().split(':')[0]:
             line.strip().split(':')[1]}
 
 
+def _group_tag_format(line):
+    key = line.strip().split(':')[0].replace('.', '_')
+    tag_list = _simple_divide(line)
+    return {key: tag_list} 
+
+
+def _pv_format(line):
+    key = line.strip().split(':')[0]
+    try:
+        tags = line.strip().split(':')[1].split(' ')[1]
+        pv = line.strip().split(':')[1].split(' ')[0]
+    except IndexError:
+        return {'error': {'pv': 'error','tag': 'error'}}
+    else:
+        return {key: {'pv': pv, 'tags': tags}}
+
+
+
 if __name__ == '__main__':
-    save_documents(DB.test, format_rdd(_relative_format,
+    save_documents(DB.relative, format_rdd(_relative_format,
         '/home/ybw_intern/tag_top/threshold_test'))
-    print DB.test.find().count()
+
+    save_documents(DB.group_tag, format_rdd(_pv_format,
+        '/home/ybw_intern/tag_top/test1'))
+
+    save_documents(DB.pv, format_rdd(_pv_format, 
+        '/home/ybw_intern/tag_top/ryan'))
 
 
